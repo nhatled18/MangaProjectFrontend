c4b4e3c4048ca808f4b7f3c174460ac1739a0731
nhatled18 <nhatled18@gmail.com>
2026-01-03 09:56:24 +0700

    remove UseEffect in navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState} from 'react';
 import { Search, Bell, User, Menu, LogOut, Settings, X } from 'lucide-react';
 import { Link, useNavigate } from 'react-router-dom';
 import { useAuth } from '../hooks/useAuth';
